8957db40c0eeed43dad0763585b7ce9b4565bfcb
VitaTro <144832571+VitaTro@users.noreply.github.com>
2024-07-02 20:36:34 +0200

    master

--- server.js
+++ server.js
@@ -5,31 +5,6 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-// app.use(express.json());
-// app.use(cors());
-
-// const routerApi = require("./api");
-// app.use("/api", routerApi);
-
-// app.use((_, res, __) => {
-//   res.status(404).json({
-//     status: "error",
-//     code: 404,
-//     message: "Use api on routes: /api/contacts",
-//     data: "Not found",
-//   });
-// });
-
-// app.use((err, _, res, __) => {
-//   console.log(err.contacts);
-//   res.status(500).json({
-//     status: "fail",
-//     code: 500,
-//     message: err.message,
-//     data: "Internal Server Error",
-//   });
-// });
-
 const MAIN_PORT = process.env.PORT || 5000;
 const DB_URL = process.env.DB_URL;
 
