2045a9508fd3fa9a8d0c6b6315b37ef6083b7fea
VitaTro <144832571+VitaTro@users.noreply.github.com>
2024-07-02 19:16:56 +0200

    hw03-finish

--- app.js
+++ app.js
@@ -10,26 +10,10 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(express.json());
 app.use(cors());
 
-// const routerApi = require("./api");
-// app.use("/api", routerApi);
-
 app.use(logger(formatsLogger));
-// app.use(cors());
-// app.use(express.json());
 
 app.use("/api/index", router);
 
-// app.use((req, res, next) => {
-//   res.status(404).json({ message: "Not found" });
-// });
-
-// app.use((err, req, res, next) => {
-//   if (err.message === "Contact not found") {
-//     res.status(404).json({ message: "Not found" });
-//   } else {
-//     res.status(500).json({ message: err.message });
-//   }
-// });
 app.use((_, res, __) => {
   res.status(404).json({
     status: "error",
