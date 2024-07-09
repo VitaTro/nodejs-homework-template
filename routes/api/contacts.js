11d994b7e27d7fa8722cc5e0438a9bd0dd202aaa
VitaTro <144832571+VitaTro@users.noreply.github.com>
2024-07-09 17:27:59 +0200

    globalna poprawka do hw-03

---
+++ routes/api/contacts.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const contactsController = require("../../controller/contactsController");
+
+router.get("/", contactsController.get);
+
+router.get("/:id", contactsController.getById);
+
+router.post("/", contactsController.create);
+
+router.delete("/:id", contactsController.removeById);
+
+router.put("/:id", contactsController.update);
+
+router.patch("/:id/favorite", contactsController.updateStatus);
+
+module.exports = router;
